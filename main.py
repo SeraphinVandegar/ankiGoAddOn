d85940513eee373345ef26b3fbe7cbd05c695cdc
seraphin <s.vandegar@student.helmo.be>
2024-12-01 08:27:17 +0100

    get cards to revise as ankiCards

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from User import User
 
 helmo = User("s.vandegar@student.helmo.be", "pppppppp")
-howest = User("seraphin.vandegar@student.howest.be", "pppppppp")
+#howest = User("seraphin.vandegar@student.howest.be", "pppppppp")
 
+print(len(helmo.getCardsToRevise()))
+card = helmo.getCardsToRevise()[0]
+card.start_timer()
+#card = Card(helmo.col, _card.id ,backend_card=_card)
+card.start_timer()
+print(type(card))
+print(card)
+print(helmo.answerCard(card, 3))
+print(len(helmo.getCardsToRevise()))
 
-helmo.answerCard(helmo.get_due_card(), 3)
+helmo.sync()

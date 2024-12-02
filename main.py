bb63ea244f2e38ab9b0c97d857b0fdc9894cffda
seraphin <s.vandegar@student.helmo.be>
2024-12-02 16:41:29 +0100

    handle http requests with flask

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
+import json
+
 from User import User
 
-helmo = User("s.vandegar@student.helmo.be", "pppppppp")
-#howest = User("seraphin.vandegar@student.howest.be", "pppppppp")
+from flask import Flask, request, jsonify, abort
 
-card = helmo.getCardsToRevise()[0]
-card.start_timer()
+app = Flask(__name__)
 
 
-fields = helmo.getCardFields(card._to_backend_card().note_id)
-print(fields)
+def getUserFromHeader(request):
+    try:
+        auth_header = request.headers.get('Authorization')
+        credentials = json.loads(auth_header)
+        return User(credentials["username"], credentials["password"])
+    except:
+        abort(401, description="Access Denied")
 
-fields = helmo.getCardFieldsFromCard(card)
-print(fields)
+
+@app.route('/get-notes', methods=['GET'])
+def get_notes():
+    user = getUserFromHeader(request)
+    response = {
+        "notes": user.getNotes()
+    }
+    return jsonify(response)
+
+
+@app.route('/get-notes-to-revise', methods=['GET'])
+def get_notes_to_revise():
+    user = getUserFromHeader(request)
+    ids = []
+    for card in user.getCardsToRevise():
+        ids.append(card.id)
+    response = {
+        "ids": ids
+    }
+    return jsonify(response)
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)

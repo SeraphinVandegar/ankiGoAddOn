7083c25ab523128d127a9cf747c0af058faebb7d
seraphin <s.vandegar@student.helmo.be>
2024-12-02 22:07:26 +0100

    answer tests -- getNotes allow you to get card ids, you can send instantiate a card by its id. Then don't forget to set the starting time, and tou can use it to answer

---
+++ tests/user/answer.py
@@ -0,0 +1,14 @@
+import time
+
+from data.User import User
+
+user = User("s.vandegar@student.helmo.be", "_")
+
+c = user.getCardsToRevise()[0]
+c.timer_started = time.time()
+print(c.__dict__)
+user.answerCard(c, 1)
+
+c2 = user.getNotes()[0]
+
+print(user.getCard(user.getCardIds()[0]).__dict__)
